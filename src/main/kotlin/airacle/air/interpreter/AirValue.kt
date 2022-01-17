b964fd51f8d0e5550b55e3886906b8d5d6bd4620
LambdaAlpha <lambdaalpha@airacle.fun>
2022-01-17 08:54:41 +0800

    refine AirValue equals, hashCode, toString
    Signed-off-by: LambdaAlpha <lambdaalpha@airacle.fun>

--- src/main/kotlin/airacle/air/interpreter/AirValue.kt
+++ src/main/kotlin/airacle/air/interpreter/AirValue.kt
@@ -5,12 +5,28 @@ sealed interface AirValue
 sealed interface PrimitiveValue : AirValue
 
 object UnitValue : PrimitiveValue {
+    override fun equals(other: Any?): Boolean {
+        return other is UnitValue
+    }
+
+    override fun hashCode(): Int {
+        return Unit.hashCode()
+    }
+
     override fun toString(): String {
         return "|"
     }
 }
 
-sealed class BoolValue(val value: Boolean) : PrimitiveValue
+sealed class BoolValue(val value: Boolean) : PrimitiveValue {
+    override fun equals(other: Any?): Boolean {
+        return other is BoolValue && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+}
 
 object TrueValue : BoolValue(true) {
     override fun toString(): String {
@@ -24,40 +40,61 @@ object FalseValue : BoolValue(false) {
     }
 }
 
-data class IntegerValue(val value: Long) : PrimitiveValue {
-    override fun toString(): String {
-        if (value >= 0) {
-            return value.toString()
-        }
-        return "0$value"
-    }
-}
-
-data class FloatValue(val value: Double) : PrimitiveValue {
-    override fun toString(): String {
-        if (value >= 0) {
-            return value.toString()
-        }
-        return "0$value"
-    }
-}
-
-data class StringValue(val value: String) : PrimitiveValue {
-    override fun toString(): String {
-        return "\"$value\""
-    }
-}
-
-data class TupleValue(val value: Array<AirValue>) : AirValue {
+class IntegerValue(val value: Long) : PrimitiveValue {
     override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        return other is IntegerValue && value == other.value
+    }
 
-        other as TupleValue
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
 
-        if (!value.contentEquals(other.value)) return false
+    override fun toString(): String {
+        return if (value >= 0) {
+            value.toString()
+        } else {
+            "0$value"
+        }
+    }
+}
 
-        return true
+class FloatValue(val value: Double) : PrimitiveValue {
+    override fun equals(other: Any?): Boolean {
+        return other is FloatValue && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+    override fun toString(): String {
+        return if (value >= 0) {
+            value.toString()
+        } else {
+            "0$value"
+        }
+    }
+}
+
+class StringValue(val value: String) : PrimitiveValue {
+    override fun equals(other: Any?): Boolean {
+        return other is StringValue && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+    override fun toString(): String = "\"$value\""
+}
+
+class TupleValue(val value: Array<AirValue>) : AirValue {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        return other is TupleValue && value.contentEquals(other.value)
     }
 
     override fun hashCode(): Int {
@@ -65,37 +102,36 @@ data class TupleValue(val value: Array<AirValue>) : AirValue {
     }
 
     override fun toString(): String {
-        val builder = StringBuilder()
-        builder.append("< ")
-        for (v in value) {
-            builder.append(v).append(" ")
-        }
-        builder.append(">")
-        return builder.toString()
+        return value.joinToString(prefix = "<", postfix = ">", separator = " ")
     }
 }
 
-data class ListValue(val value: MutableList<AirValue>) : AirValue {
+class ListValue(val value: MutableList<AirValue>) : AirValue {
+    override fun equals(other: Any?): Boolean {
+        return other is ListValue && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     override fun toString(): String {
-        val builder = StringBuilder()
-        builder.append("[ ")
-        for (v in value) {
-            builder.append(v).append(" ")
-        }
-        builder.append("]")
-        return builder.toString()
+        return value.joinToString(prefix = "[", postfix = "]", separator = " ")
     }
 }
 
-data class MapValue(val value: MutableMap<AirValue, AirValue>) : AirValue {
+class MapValue(val value: MutableMap<AirValue, AirValue>) : AirValue {
+    override fun equals(other: Any?): Boolean {
+        return other is MapValue && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     override fun toString(): String {
-        val builder = StringBuilder()
-        builder.append("{ ")
-        for (v in value.entries) {
-            builder.append(v.key).append(" ")
-            builder.append(v.value).append(" ")
-        }
-        builder.append("}")
-        return builder.toString()
+        return value.entries.joinToString(prefix = "{", postfix = "}", separator = " ", transform = {
+            "${it.key}:${it.value}"
+        })
     }
 }
