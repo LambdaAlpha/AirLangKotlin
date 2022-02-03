b02e5a7613965fe2adbe37af89e319035c8a4157
LambdaAlpha <lambdaalpha@airacle.fun>
2022-02-03 18:18:35 +0800

    implement interpreter (WIP)
    - boolean operations
    - int and number operations
    - parser operations
    - comparator operations
    - type operations
    
    Signed-off-by: LambdaAlpha <lambdaalpha@airacle.fun>

--- src/main/kotlin/airacle/air/interpreter/AirInterpreter.kt
+++ src/main/kotlin/airacle/air/interpreter/AirInterpreter.kt
@@ -4,6 +4,7 @@ import airacle.air.api.IAirInterpreter
 import airacle.air.api.IAirLexer
 import airacle.air.api.IAirParser
 import airacle.air.lexer.AirToken
+import airacle.air.parser.AirParserConfig
 
 class AirInterpreter(
     val version: AirInterpreterVersion,
@@ -55,7 +56,50 @@ class AirInterpreter(
     }
 
     override fun interpret(value: AirValue): AirValue {
-        // TODO: 2022/2/3 impl
+        if (value !is TupleValue) {
+            return value
+        }
+        if (value.value.isNotEmpty()) {
+            val tuple = value.value
+            val keyword = tuple[0]
+            if (keyword is StringValue) {
+                when (keyword.value) {
+                    AirParserConfig.NOT -> return booleans.not(interpret(tuple[1]))
+                    AirParserConfig.AND -> return booleans.and(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.OR -> return booleans.or(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.XOR -> return booleans.xor(interpret(tuple[1]), interpret(tuple[2]))
+
+                    AirParserConfig.LE -> return comparators.le(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.LT -> return comparators.lt(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.GE -> return comparators.ge(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.GT -> return comparators.gt(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.EQ -> return comparators.eq(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.NE -> return comparators.ne(interpret(tuple[1]), interpret(tuple[2]))
+
+                    AirParserConfig.ADD -> return numbers.add(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.SUBTRACT -> return numbers.subtract(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.MULTIPLY -> return numbers.multiply(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.DIVIDE -> return numbers.divide(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.REMAINDER -> return numbers.remainder(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.DIVIDE_AND_REMAINDER -> return numbers.divideAndRemainder(
+                        interpret(tuple[1]),
+                        interpret(tuple[2])
+                    )
+
+                    AirParserConfig.BIT_AND -> return ints.bitAnd(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.BIT_OR -> return ints.bitOr(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.BIT_XOR -> return ints.bitXor(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.SHIFT_LEFT -> return ints.shiftLeft(interpret(tuple[1]), interpret(tuple[2]))
+                    AirParserConfig.SHIFT_RIGHT -> return ints.shiftRight(interpret(tuple[1]), interpret(tuple[2]))
+
+                    AirParserConfig.TYPE_OF -> return types.typeOf(interpret(tuple[1]))
+                    AirParserConfig.TYPE_CAST -> return types.toType(interpret(tuple[1]), interpret(tuple[2]))
+
+                    AirParserConfig.ENCODE_TO_STRING -> return parsers.encodeToString(interpret(tuple[1]))
+                    AirParserConfig.DECODE_FROM_STRING -> return parsers.decodeFromString(interpret(tuple[1]))
+                }
+            }
+        }
         return value
     }
 }
--- src/main/kotlin/airacle/air/parser/AirParserConfig.kt
+++ src/main/kotlin/airacle/air/parser/AirParserConfig.kt
@@ -4,19 +4,80 @@ import airacle.air.interpreter.AirValue
 import airacle.air.interpreter.StringValue
 
 object AirParserConfig : IAirParserConfig {
+    const val EXIT = "exit"
+    const val QUIT = "quit"
+
+    const val NOT = "not"
+    const val AND = "and"
+    const val OR = "or"
+    const val XOR = "xor"
+
+    const val LE = "le"
+    const val LT = "lt"
+    const val GE = "ge"
+    const val GT = "gt"
+    const val EQ = "eq"
+    const val NE = "ne"
+
+    const val ADD = "add"
+    const val SUBTRACT = "sub"
+    const val MULTIPLY = "mul"
+    const val DIVIDE = "div"
+    const val REMAINDER = "rem"
+    const val DIVIDE_AND_REMAINDER = "divAndRem"
+
+    const val BIT_AND = "bitAnd"
+    const val BIT_OR = "bitOr"
+    const val BIT_XOR = "bitXor"
+    const val SHIFT_LEFT = "shl"
+    const val SHIFT_RIGHT = "shr"
+
+    const val ENCODE_TO_STRING = "toString"
+    const val DECODE_FROM_STRING = "fromString"
+
+    const val TYPE_OF = "typeOf"
+    const val TYPE_CAST = "toType"
+
+    const val NOP = "nop"
+    const val IF = "if"
+    const val FOR = "for"
+    const val WHILE = "while"
+    const val FUNCTION = "function"
+    const val RETURN = "return"
+    const val CALL = "call"
+
+    const val READ = "read"
+    const val WRITE = "write"
+
     override fun paramLength(value: AirValue): Int {
         if (value is StringValue) {
             return when (value.value) {
-                "if" -> 3
-                "for" -> 4
-                "while" -> 2
-                "function" -> 2
-                "return" -> 1
-                "call" -> 2
-                "read", "." -> 1
-                "assign", "=" -> 2
-                "le", "lt", "ge", "gt", "eq", "ne" -> 2
-                "exit", "quit" -> 0
+                EXIT, QUIT -> 0
+
+                NOT -> 1
+                AND, OR, XOR -> 2
+
+                LE, LT, GE, GT, EQ, NE -> 2
+
+                ADD, SUBTRACT, MULTIPLY, DIVIDE, REMAINDER, DIVIDE_AND_REMAINDER -> 2
+
+                BIT_AND, BIT_OR, BIT_XOR, SHIFT_LEFT, SHIFT_RIGHT -> 2
+
+                TYPE_OF -> 1
+                TYPE_CAST -> 2
+
+                ENCODE_TO_STRING, DECODE_FROM_STRING -> 1
+
+                NOP -> 0
+                IF -> 3
+                FOR -> 4
+                WHILE -> 2
+                FUNCTION -> 2
+                RETURN -> 1
+                CALL -> 2
+
+                READ -> 1
+                WRITE -> 2
                 else -> -1
             }
         }
